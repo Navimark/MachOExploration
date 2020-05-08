ae498eabf2e9514d571677ca71c47175cea037ec
chenzheng <chenzheng@qiushibaike.com>
2020-05-08 15:59:18 +0800

    init

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by chenzheng on 2020/5/8.
+//  Copyright © 2020 CST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MachOExploration/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  MachOExploration
+//
+//  Created by chenzheng on 2020/5/8.
+//  Copyright © 2020 CST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <mach-o/loader.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        /**
+         读取
+         */
+    }
+    return 0;
+}
